51060c28d856a4f9199d451f20835f47c2616f47
Jules0315 <31406323+Jules0315@users.noreply.github.com>
2024-04-04 22:08:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Find the iframe element
-    var iframe = document.querySelector('iframe');
-    // Get the current src attribute value
-    var src = iframe.getAttribute('src');
-    // Append a unique timestamp query string to the src
-    var newSrc = src + '&timestamp=' + new Date().getTime();
-    // Set the modified src attribute back to the iframe
-    iframe.setAttribute('src', newSrc);
-
-    console.log('Iframe src updated to bypass cache');
+    var refreshButton = document.getElementById('refreshButton');
+    refreshButton.addEventListener('click', function() {
+        var iframe = document.querySelector('iframe');
+        var src = iframe.src.split('&timestamp=')[0]; // Remove any existing timestamp
+        iframe.src = src + '&timestamp=' + new Date().getTime(); // Append new timestamp
+        console.log('Iframe reloaded manually.');
+    });
 });
 
